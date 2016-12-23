ee04a8d63737bdb78c9b06f08821d6a9af5a8968
Marcin <zielinski.marcin22@gmail.com>
2016-12-23 21:03:02 +0100

    testy

--- test/goserver/game/GameTest.java
+++ test/goserver/game/GameTest.java
@@ -40,12 +40,14 @@ public class GameTest
     {
     	s = new Server();
     	s.startServer();
+    	s.setPort(3333);
         game = new Game();
-        socket = new Socket("localhost", 5556);
+        socket = new Socket("localhost", 3333);
         out = new PrintWriter(socket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         clientHandler = new ClientHandler(socket, game);
     }
+   
     
     @Test
     public void testChooseOpponent()
@@ -54,14 +56,43 @@ public class GameTest
 			game.addPlayer("Ania", clientHandler);
 		} catch (NameContainsSpaceException | EmptyNameException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
         assertTrue(game.chooseOpponent("Ania", clientHandler.getPlayer()));
+     
     } 
+    
+    @Test
+    public void testGetPlayerNamed(){
+    	try {
+			game.addPlayer("Ania", clientHandler);
+		} catch (NameContainsSpaceException | EmptyNameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	assertEquals(game.getPlayerNamed("Ania").getName(), "Ania");
+    }
+    
+    @Test
+    public void testDeletePlayer(){
+    	try {
+			game.addPlayer("Ania", clientHandler);
+		} catch (NameContainsSpaceException | EmptyNameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	assertTrue(game.chooseOpponent("Ania", clientHandler.getPlayer()));
+    	game.deletePlayer(game.getPlayerNamed("Ania"));
+    	assertFalse(game.chooseOpponent("Ania", clientHandler.getPlayer()));
+    	
+    	
+    }
+    
     
     
     @After
     public void cleanUp(){
     	s.stopServer();
+    	   game.deletePlayer(game.getPlayerNamed("Ania"));
     }
 }
